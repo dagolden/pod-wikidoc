360417eca3b5fe9e4ae6d9e3d22973567a5c2585
David Golden <dagolden@cpan.org>
2006-09-09 15:48:04 +0000

    r3761@vesta (orig r287):  david | 2006-06-09 01:58:40 -0400   r1155@vesta:  david | 2005-09-21 22:58:56 -0400   [feature] Revised Build.PL to auto generate .pod
      [meta] updated
      
      r1156@vesta:  david | 2005-09-22 07:13:30 -0400
      [feature] added Cookbook
      r1157@vesta:  david | 2005-09-22 07:30:06 -0400
      [doc] Updated documentation
      r1158@vesta:  david | 2005-09-22 11:38:55 -0400
      [feature] Added "Pod generated by" message and fixed up tests to account for
      the extraneous line vs what's in the casefiles
      
      [bug] Fixed handling codeblock immediately after a paragraph; now is properly
      separated even without extra blank line between
      
      [doc] Added more documentation
      
      [meta] Updated Build.PL subclass slightly
      
      
      r1159@vesta:  david | 2005-09-22 11:44:59 -0400
      [meta] updated
      
      [doc] updated
      
     
     
     
    
    
    git-svn-id: https://dagolden.googlecode.com/svn/Pod-WikiDoc/trunk@299 dfce27d5-b31c-0410-bb09-030b4413eeba

---
+++ lib/Pod/WikiDoc/Cookbook.pm
@@ -0,0 +1,105 @@
+package Pod::Wikidoc::Cookbook;
+# Not really a .pm file, but holds wikidoc which will be
+# turned into .pod by the Build.PL
+1;
+__END__
+
+=begin wikidoc
+
+= NAME
+
+Pod::WikiDoc::Cookbook - Examples of Pod::WikiDoc usage
+
+= DESCRIPTION
+
+This file contains some lengthier examples of ways to use [Pod::WikiDoc]
+or to integrate [Pod::WikiDoc] with other tools.
+
+= BASIC RECIPES
+
+== Subclassing Module::Build to automatically generate Pod from wikidoc
+
+With a little extra work in the Build.PL file, Pod::WikiDoc
+can work easily with [Module::Build] to extract wikidoc into .pod
+files automatically during the distribution process
+
+The Build.PL file below subclasses [Module::Build] with three functions:
+
+* ACTION_wikidoc -- adds a new {Build wikidoc} action that extracts Pod
+and wikidoc from all .pm files in the {lib} directory and adds them to
+the MANIFEST
+* ACTION_testpod -- adds a dependency on the {wikidoc} action to regenerate
+.pod files before testing them
+* ACTION_distdir -- adds a dependency on the {wikidoc} action to regenerate
+.pod files before bundling up a distribution
+
+By making wikidoc extraction part of the {distdir} action, users installing
+the distribution will receive it with .pod files already created, and will 
+not need to have [Pod::WikiDoc] installed themselves.
+
+    use Module::Build;
+
+    my $class = Module::Build->subclass(
+        class => "Module::Build::WikiDoc",
+        code => <<'SUBCLASS',
+
+        sub ACTION_wikidoc {
+            my $self = shift;
+            eval "use Pod::WikiDoc";
+            if ( $@ eq '' ) {
+                my $parser = Pod::WikiDoc->new({comment_doc => 1});
+                for my $src ( keys %{ $self->find_pm_files() } ) {
+                    (my $tgt = $src) =~ s{\.pm$}{.pod};
+                    $parser->filter( {
+                        input   => $src,
+                        output  => $tgt,
+                    });
+                    print "Creating $tgt\n";
+                    $self->_add_to_manifest( 'MANIFEST', $tgt );
+                }
+            }
+            else {
+                warn "Pod::WikiDoc not available. Skipping wikidoc.";
+            }
+        }
+
+        sub ACTION_testpod {
+            my $self = shift;
+            $self->depends_on('wikidoc');
+            $self->SUPER::ACTION_testpod;
+        }
+
+        sub ACTION_distdir {
+            my $self = shift;
+            $self->depends_on('wikidoc');
+            $self->SUPER::ACTION_distdir;
+        }
+
+    SUBCLASS
+    );
+                
+    $class->new( 
+        # regular Module::Build options
+    )->create_build_script;
+
+= AUTHOR
+
+David A Golden (DAGOLDEN)
+
+dagolden@cpan.org
+
+[http://dagolden.com/]
+
+= COPYRIGHT
+
+Copyright (c) 2005 by David A Golden
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=end wikidoc
+
+=cut
