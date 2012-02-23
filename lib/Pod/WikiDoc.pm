b38c0b6d941504f28ac1555a22184885ea555f1a
David Golden <dagolden@cpan.org>
2012-02-23 10:38:28 -0500

    moving around Pod generation for dzil

--- lib/Pod/WikiDoc.pm
+++ lib/Pod/WikiDoc.pm
@@ -1,8 +1,8 @@
 package Pod::WikiDoc;
 use strict;
 use warnings;
-use vars qw($VERSION );
-$VERSION     = '0.19';
+# ABSTRACT: Generate Pod from inline wiki style text 
+# VERSION
 
 use 5.006;
 use Carp;
@@ -15,14 +15,6 @@ use Pod::WikiDoc::Parser;
 #--------------------------------------------------------------------------#
 
 =begin wikidoc
-
-= NAME
-
-Pod::WikiDoc - Generate Pod from inline wiki style text 
-
-= VERSION
-
-This documentation refers to version %%VERSION%%.
 
 = SYNOPSIS
 
@@ -883,39 +875,6 @@ conflict.
 to generate a {README} with the {create_readme} option or to find a module 
 abstract.  Set the abstract manually in the {Build.PL} file with the
 {dist_abstract} option.
-
-= BUGS
-
-Please report bugs or feature requests using the CPAN Request Tracker.
-Bugs can be submitted using the web interface at
-[http://rt.cpan.org/Public/Dist/Display.html?Name=Pod-WikiDoc]
-
-When submitting a bug or request, please include a test-file or a patch to an
-existing test-file that illustrates the bug or desired feature.
-
-= AUTHOR
-
-David A. Golden (DAGOLDEN)
-
-= COPYRIGHT AND LICENSE
-
-Copyright (c) 2005, 2006, 2007 by David A. Golden
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at 
-[http://www.apache.org/licenses/LICENSE-2.0]
-
-Files produced as output though the use of this software, including
-generated copies of boilerplate templates provided with this software,
-shall not be considered Derivative Works, but shall be considered the
-original work of the Licensor.
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 
 =end wikidoc
 
